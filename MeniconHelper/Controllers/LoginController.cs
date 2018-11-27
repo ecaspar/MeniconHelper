64da021593230470c08dea162bb8c52d271ef883
Eric CASPAR <eric.caspar@viacesi.fr>
2018-11-27 15:36:40 +0100

    test45

--- MeniconHelper/Controllers/LoginController.cs
+++ MeniconHelper/Controllers/LoginController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 using MeniconHelper.App_LocalResources;
 
@@ -24,27 +21,23 @@ namespace MeniconHelper.Controllers
         public ActionResult Verify(person loginPerson)
         {
             MeniconHelperEntities meniconHelperEntities = new MeniconHelperEntities();
-            
-            foreach(person p in meniconHelperEntities.person)
+            foreach (person p in meniconHelperEntities.person)
             {
                 if (p.username == loginPerson.username)
                 {
-                    if(GenerateMD5(loginPerson.password_default) == p.password_default)
+                    if (GenerateMD5(loginPerson.password_default) == p.password_default)
                     {
                         Session["User"] = p;
-                        meniconHelperEntities.Dispose();
                         return RedirectToAction("../Home/Index");
                     }
                     else
-                    { 
+                    {
                         ViewBag.Message = GlobalRes.incorrectPassword;
-                        meniconHelperEntities.Dispose();
                         return View("Index");
-                    }    
+                    }
                 }
-            }
+            } 
             ViewBag.Message = GlobalRes.incorrectUsername;
-            meniconHelperEntities.Dispose();
             return View("Index");
         }
         public string GenerateMD5(string rawString)
