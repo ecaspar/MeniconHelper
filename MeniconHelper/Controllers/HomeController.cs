f012fadedf744c9aef45c234ae1dd26adc548514
Takachi67 <LucasHebinger67@gmail.com>
2018-11-28 10:01:48 +0100

    Menu button add

--- MeniconHelper/Controllers/HomeController.cs
+++ MeniconHelper/Controllers/HomeController.cs
@@ -30,15 +30,14 @@ namespace MeniconHelper.Controllers
         {
             List<ListIncident> list = new List<ListIncident>();
             
-
             MeniconHelperEntities meniconHelperEntities = new MeniconHelperEntities();
-            ListIncident listIncident = new ListIncident();
-            /* foreach(incident i in meniconHelperEntities.incident)
+
+            foreach(incident i in meniconHelperEntities.incident)
             {
                 ListIncident listIncident = new ListIncident();
 
                 listIncident.Reference = i.incident_code;
-                listIncident.Image = "Une image";
+                listIncident.Image = meniconHelperEntities.document.First(x=>x.id_anomaly == i.id_anomaly).name;
                 listIncident.Description = i.description;
                 listIncident.Supervisor = "?";
                 listIncident.Date = i.date_create;
@@ -48,33 +47,7 @@ namespace MeniconHelper.Controllers
                 listIncident.Engine = i.engine.name;
 
                 list.Add(listIncident);
-            } */
-
-            listIncident.Reference = "TQUA1";
-            listIncident.Image = "../Content/Images/Incident.jpg";
-            listIncident.Description = "Une description";
-            listIncident.Supervisor = "L. Hebinger";
-            listIncident.Date = DateTime.Now;
-            listIncident.Declarant = "E. Caspar";
-            listIncident.Type = "Qualité";
-            listIncident.Area = "Ligne 1";
-            listIncident.Engine = "Emballage";
-
-            list.Add(listIncident);
-
-            ListIncident listIncident2 = new ListIncident();
-
-            listIncident2.Reference = "TQUA2";
-            listIncident2.Image = "../Content/Images/Incident2.jpg";
-            listIncident2.Description = "Une description";
-            listIncident2.Supervisor = "L. Hebinger";
-            listIncident2.Date = DateTime.Now;
-            listIncident2.Declarant = "E. Caspar";
-            listIncident2.Type = "Qualité";
-            listIncident2.Area = "Ligne 1";
-            listIncident2.Engine = "Emballage";
-
-            list.Add(listIncident2);
+            }
 
             return list;
         }
