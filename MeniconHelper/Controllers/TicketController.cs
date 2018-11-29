86dc08e840ffdf0d8ad553ae6c99ff1a4d18820c
Eric CASPAR <eric.caspar@viacesi.fr>
2018-11-29 11:58:13 +0100

    Private

--- MeniconHelper/Controllers/TicketController.cs
+++ MeniconHelper/Controllers/TicketController.cs
@@ -28,7 +28,7 @@ namespace MeniconHelper.Controllers
                 return View("../Login/Index");
         }
 
-        public ListIncident LoadTicket()
+        private ListIncident LoadTicket()
         {
 
             ListIncident listIncident = new ListIncident();
@@ -65,7 +65,7 @@ namespace MeniconHelper.Controllers
 
         }
 
-        public List<ListTask> LoadTask()
+        private List<ListTask> LoadTask()
         {
 
             List<ListTask> list = new List<ListTask>();
