e284c433553d21b8cae8a56aa5f080984312bd55
Eric CASPAR <eric.caspar@viacesi.fr>
2018-12-19 19:18:57 +0100

    avant réunion

--- MeniconHelper/Controllers/HomeController.cs
+++ MeniconHelper/Controllers/HomeController.cs
@@ -60,7 +60,7 @@ namespace MeniconHelper.Controllers
         }
 
         //Controller for the view Views/Home/MyDeclarations
-        public ActionResult MyDeclarations()
+        public ActionResult MyDeclarations() 
         {
             //Someone who isn't logged can't access to this view.
             if (Session["User"] != null)
